ff6cd0917f513944ef52d1603ced7e0bca6f951a
Oleg Lebedev <oleg@lebedev.au>
2025-11-16 20:07:14 +1100

    macOS: add ability to make titlebar unified
    See: https://github.com/olebedev/winit/commit/416c109004aba64cdf674a1e5688d08f6ddce986

--- winit/src/conversion.rs
+++ winit/src/conversion.rs
@@ -105,7 +105,8 @@ pub fn window_attributes(
             )
             .with_fullsize_content_view(
                 settings.platform_specific.fullsize_content_view,
-            );
+            )
+            .with_unified_titlebar(settings.platform_specific.unified_titlebar);
     }
 
     #[cfg(target_os = "linux")]
